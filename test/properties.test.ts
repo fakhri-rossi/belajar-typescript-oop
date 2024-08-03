f54d89eb4369d548bbac7077e2add8a149f6f386
Fakhri Rossi <rossifakhri@gmail.com>
2024-08-04 02:31:58 +0700

    Method

--- test/properties.test.ts
+++ test/properties.test.ts
@@ -8,11 +8,21 @@ describe("Properties", () => {
       this.id = id;
       this.name = name;
     }
+
+    sayHello(name: string): void {
+      console.info(`Hello ${name}, my name is ${this.name}`);
+    }
   }
+
   it("should can have properties", () => {
     const customer = new Customer(1, "John");
     customer.age = 20;
 
     console.info(customer);
+  });
+
+  it("should can have methods", () => {
+    const customer = new Customer(1, "Rossi");
+    customer.sayHello("Andi");
   });
 });
