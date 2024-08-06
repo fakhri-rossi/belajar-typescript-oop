e874bcb167519e12ef4bdf6963a4665ea2ec6b44
Fakhri Rossi <rossifakhri@gmail.com>
2024-08-06 15:21:59 +0700

    Type Cast

--- test/polymorphism.test.ts
+++ test/polymorphism.test.ts
@@ -8,7 +8,29 @@ describe("Polymorphism", () => {
   class VicePresident extends Manager {}
 
   function sayHello(person: Employee) {
-    console.info(`Hello ${person.name}`);
+    if (person instanceof VicePresident) {
+      const vp = person as VicePresident;
+      console.info(`Hello VP ${vp.name}`);
+    } else if (person instanceof Manager) {
+      const manager = person as Manager;
+      console.info(`Hello Manager ${manager.name}`);
+    } else {
+      const employee = person as Employee;
+      console.info(`Hello Employee ${employee.name}`);
+    }
+  }
+
+  function sayHelloWrong(person: Employee) {
+    if (person instanceof Manager) {
+      const manager = person as Manager;
+      console.info(`Hello Manager ${manager.name}`);
+    } else if (person instanceof VicePresident) {
+      const vp = person as VicePresident;
+      console.info(`Hello VP ${vp.name}`);
+    } else {
+      const employee = person as Employee;
+      console.info(`Hello Employee ${employee.name}`);
+    }
   }
 
   it("should support polymorphism", () => {
@@ -26,5 +48,11 @@ describe("Polymorphism", () => {
     sayHello(new Employee("Eko"));
     sayHello(new Manager("Andi"));
     sayHello(new VicePresident("Joko"));
+  });
+
+  it("should support wrong method parameter polymorphism", () => {
+    sayHelloWrong(new Employee("Eko"));
+    sayHelloWrong(new Manager("Andi"));
+    sayHelloWrong(new VicePresident("Joko"));
   });
 });
